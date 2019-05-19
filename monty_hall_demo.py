cc28e0c2c312607e39265f39a19027bc115989fb
The third meow <jsheaandcats@gmail.com>
2019-05-19 21:01:52 +1200

    write run without switch function

--- monty_hall_demo.py
+++ monty_hall_demo.py
@@ -1,10 +1,19 @@
-
+import random as rand
 
 def run_with_switch():
     return 1
 
 def run_without_switch():
-    return 1
+    doors = [False, False, False]
+    doors[rand.randrange(3)] = True
+
+    choice = rand.randrange(3)
+    if doors[choice]:
+        return 1
+    else:
+        return 0
+
+
 
 def total(l):
     """ Total all the elements in a list """
@@ -18,15 +27,15 @@ def main():
     with_switch = []
     without_switch = []
 
-    for i in range(100):
+    for i in range(10000):
         with_switch.append(run_with_switch())
         without_switch.append(run_without_switch())
 
     total_wins_with_switch = total(with_switch)
-    win_percent_with_switch = (total_wins_with_switch / 100) * 100
+    win_percent_with_switch = (total_wins_with_switch / len(with_switch)) * 100
 
     total_wins_without_switch = total(without_switch)
-    win_percent_without_switch = (total_wins_without_switch / 100) * 100
+    win_percent_without_switch = (total_wins_without_switch / len(without_switch)) * 100
 
     print()
     print(f"Win percent when consistantly sticking to first choice: {win_percent_without_switch}%")
